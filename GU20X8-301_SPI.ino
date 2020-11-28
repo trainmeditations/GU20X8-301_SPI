df86bab31f613ae6118e404d941b21a44dc1cd65
Shaun Bouckaert <shaun@train-meditations.com>
2020-11-28 12:40:15 +1000

    Initial Commit

---
+++ GU20X8-301_SPI.ino
@@ -0,0 +1,122 @@
+#include <SPI.h>
+
+/* 
+  Shift Register latches on rising edge
+  VFD on falling edge 
+*/
+
+// 74HC595 and VFD latch pin
+#define latchPin PIN_SPI_SS
+
+/*
+ * VFD Pins
+ */
+
+// VFD Reset Pin
+#define rstPin 8
+
+// VFD Enable Pin
+#define enPin 4
+
+// VFD Busy Pin
+#define busyPin 7
+
+void drawDemo() {
+  static byte brightness=0;
+  const byte img1[2][20]  = {
+    { //blue
+      0xFC, 0xFC, 0xFC, 0xF8, 0xF0,
+      0xF0, 0xE0, 0xE4, 0xC0, 0x80,
+      0x80, 0xC0, 0xE4, 0xE0, 0xF0,
+      0xF0, 0xF8, 0xFC, 0xFC, 0xFC
+    },
+    { //red
+      0x3F, 0x3F, 0x3F, 0x39, 0x30,
+      0x31, 0x23, 0x27, 0x09, 0x03,
+      0x03, 0x09, 0x27, 0x23, 0x31,
+      0x30, 0x39, 0x3F, 0x3F, 0x3F
+    }
+  };
+  const byte img2[2][20] = {
+    { //blue
+      0xFC, 0xFC, 0xFC, 0x9C, 0x0C,
+      0x8C, 0xC4, 0xE4, 0xC8, 0x80,
+      0x80, 0xC8, 0xE4, 0xC4, 0x8C,
+      0x0C, 0x9C, 0xFC, 0xFC, 0xFC
+    },
+    { //red
+      0x3F, 0x3F, 0x3F, 0x1F, 0x0F,
+      0x0F, 0x07, 0x27, 0x01, 0x03,
+      0x03, 0x01, 0x27, 0x07, 0x0F,
+      0x0F, 0x1F, 0x3F, 0x3F, 0x3F
+    }
+  };
+
+  drawDisplay(img1);
+  delay(2000);
+  drawDisplay(img2);
+  delay(2000);
+}
+
+void setBrightness(byte brightness) {
+  SPI.transfer(0x3F);
+  SPI.transfer(brightness);
+  toggleLatch();
+
+}
+
+void drawBlue(const byte img[20]){
+  for (byte i = 0; i<20; i++) {
+    SPI.transfer(i+0x80);//address with 0x80 offset for data/control
+    SPI.transfer(img[i]);
+    toggleLatch();
+  }
+}
+
+void drawRed(const byte img[20]){
+  for (byte i=0; i<20; i++) {
+    SPI.transfer(i+0x80+0x20);//address with 0x80 offset for D/C and 0x20 for red
+    SPI.transfer(img[i]);
+    toggleLatch();
+  }
+}
+
+void drawDisplay(const byte img[2][20]){
+  drawBlue(img[0]);
+  drawRed(img[1]);
+}
+
+inline void toggleLatch() {
+  digitalWrite(latchPin, HIGH);
+  delay(1);
+  digitalWrite(latchPin, LOW);
+  while(digitalRead(busyPin)==HIGH);
+}
+
+void setup() {
+
+  //disable display
+  digitalWrite(enPin, LOW);
+  pinMode(enPin, OUTPUT);
+  
+  pinMode(busyPin, INPUT);
+
+  //setup SPI
+  pinMode(latchPin, OUTPUT);
+  SPI.begin();
+
+  //reset display
+  pinMode(rstPin, OUTPUT);
+  digitalWrite(rstPin, HIGH);
+
+  setBrightness(0x03);
+
+  //enable display
+  digitalWrite(enPin, HIGH);
+}
+
+void loop() {
+
+ drawDemo();
+
+}
